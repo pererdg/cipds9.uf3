96518099f5799b8a0555ffc37d34406e2acb3036
pererdg <101477235+pererdg@users.noreply.github.com>
2023-01-21 13:10:03 +0100

    Versió inicial

--- UF3 Activitats/src/cipds9/uf3/activitat6/Reciever.java
+++ UF3 Activitats/src/cipds9/uf3/activitat6/Reciever.java
@@ -2,26 +2,46 @@ package cipds9.uf3.activitat6;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 
+/**
+ * Classe per establir una connexió UDP com a servidor.
+ * Les dades que es reben pel socket UDP es presenten en consola.
+ * 
+ * @author perer
+ *
+ */
 public class Reciever {
 
-	private static int PORT = 6500;
-	private static String IP = "127.0.0.1";
-	
+	// Constants de configuració
+	private static final int PORT = 6500;
+
+	/**
+	 * Punt d'entrada al programa
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		String msg;
 		byte[] buffer = new byte[2048];
 		
+		// Crear paquet UDP i socket per rebre les dades.
+		// Al socket no se li especifica la IP, per tant, podrà rebre per totes les
+		// interfícies de la màquina.
 		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-		DatagramSocket socket = new DatagramSocket(PORT, InetAddress.getByName(IP));
+		DatagramSocket socket = new DatagramSocket(PORT);
+		
+		System.out.println("RECIEVER");
 		
 		do {
+			// Escoltar i esperar fins que arribin dades
 			socket.receive(packet);
+			// Transformar a String i presentar
 			msg = new String(buffer, 0, packet.getLength());
 			System.out.println(msg);
 		} while (!msg.equalsIgnoreCase("end"));
 		
+		// Tancar recursos
 		socket.close();
 	}
 }
--- UF3 Activitats/src/cipds9/uf3/activitat6/Sender.java
+++ UF3 Activitats/src/cipds9/uf3/activitat6/Sender.java
@@ -5,30 +5,49 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
+/**
+ * Classe per establir connexió UDP com a client.
+ * Llegeix dades de consola i les envia pel socket UDP.
+ * 
+ * @author perer
+ *
+ */
 public class Sender {
 
-	private static int PORT = 6500;
-	private static String IP = "127.0.0.1";
+	// Constants de configuració
+	private static final int PORT = 6500;
+	private static final String IP = "127.0.0.1";
 	
+	/**
+	 * Punt d'entrada al programa
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		String msg;
 		byte[] buffer;
 		
+		// Crear socket UDP per enviar dades
 		DatagramSocket socket = new DatagramSocket();
 		
+		System.out.println("SENDER");
 		Scanner in = new Scanner(System.in);
 		
 		do {
+			// Legir de consola i validar longitud
 			msg = in.nextLine();
 			buffer = msg.getBytes();
 			if (buffer.length>2048) {
 				System.out.println("Missatge massa llarg");
 			} else  {
+				// Crear paquet UDP i enviar utilitzant el socket UDP
 				DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(IP), PORT);
 				socket.send(packet);
 			}
 		} while (!msg.equalsIgnoreCase("end"));
 
+		// Tancar recursos
 		in.close();
 		socket.close();
 	}
